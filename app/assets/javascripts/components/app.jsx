5941548211f539d173a6a0a8e8f1bf85bd19e6ba
Merlyn <curious.attempt.bunny@gmail.com>
2017-06-10 21:05:34 -0700

    Simple bootstrap theme with signin / signout.

--- app/assets/javascripts/components/app.jsx
+++ app/assets/javascripts/components/app.jsx
@@ -1,11 +1,53 @@
 class App extends React.Component {
-    render() {
+    renderAuthButton() {
         const { current_user } = this.props
 
         if (current_user) {
-            return <span>Ohai {current_user.email}! <a href="/signout">Sign out</a></span>
+            return (
+                <form className="navbar-form navbar-right" method="get" action="/signout">
+                    <button type="submit" className="btn btn-default">Sign out</button>
+                </form>
+            )
         } else {
-            return <span>Ohai. Who are you? <a href="/auth/google_oauth2">Sign in with Google</a></span>
+            return (
+                <form className="navbar-form navbar-right" method="get" action="/auth/google_oauth2">
+                    <button type="submit" className="btn btn-success">Sign in with Google</button>
+                </form>
+            )
         }
+    }
+
+    render() {
+        return (
+            <div>
+
+    <nav className="navbar navbar-inverse navbar-fixed-top">
+      <div className="container">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a className="navbar-brand" href="/">dokku-rails-omniauth-bootstrap-react</a>
+        </div>
+        <div id="navbar" className="navbar-collapse collapse">
+          { this.renderAuthButton() }
+        </div>
+      </div>
+    </nav>
+
+    <div className="container">
+
+      <hr/>
+
+      <footer>
+        
+      </footer>
+    </div> 
+
+            </div>
+        )
     }
 }
