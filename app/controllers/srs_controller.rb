8bf25005ccb32c4e49b9609b6730b72588959f94
Merlyn <curious.attempt.bunny@gmail.com>
2017-09-10 17:33:33 -0700

    Revise mock srs/status response structure.

--- app/controllers/srs_controller.rb
+++ app/controllers/srs_controller.rb
@@ -1,7 +1,18 @@
 class SrsController < ApplicationController
     def status
+        srs_level_totals = [1, 2, 4, 8, 16, 32]
         status = {
-            srs_level_counts: [1, 2, 4, 8, 16, 32]
+            srs_level_totals: srs_level_totals,
+            levels: {
+                apprentice: {
+                    srs_level_totals: srs_level_totals[0...4],
+                    total: srs_level_totals[0] + srs_level_totals[1] + srs_level_totals[2] + srs_level_totals[3]
+                },
+                guru: {
+                    srs_level_totals: srs_level_totals[4...6],
+                    total: srs_level_totals[4] + srs_level_totals[5]
+                }
+            }
         }
 
         render json: status
